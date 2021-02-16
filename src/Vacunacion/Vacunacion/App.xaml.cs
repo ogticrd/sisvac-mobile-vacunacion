8dc9617a4bc9d9b397091370a90b35dc4ee115df
luismts <deivi_0326@hotmail.com>
2021-02-15 21:18:12 -0400

    Login page added

--- src/Vacunacion/Vacunacion/App.xaml.cs
+++ src/Vacunacion/Vacunacion/App.xaml.cs
@@ -5,6 +5,7 @@ using Microsoft.AppCenter.Crashes;
 using Prism;
 using Prism.Ioc;
 using Prism.Navigation;
+using Vacunacion.Pages.Login;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -19,17 +20,20 @@ namespace Vacunacion
         protected override async void OnInitialized()
         {
             InitializeComponent();
+
             AppCenter.Start("android=89389cb2-d03e-4a44-a48c-d1e4ce35f9f1;" +
                   "uwp={Your UWP App secret here};" +
                   "ios={Your iOS App secret here}",
                   typeof(Analytics), typeof(Crashes));
-            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+
+            await NavigationService.NavigateAsync("NavigationPage/LoginPage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<HomePage>();
+            containerRegistry.RegisterForNavigation<LoginPage>();
         }
     }
 }
---
+++ src/Vacunacion/Vacunacion/ViewModels/Login/LoginPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vacunacion.ViewModels.Login
+{
+    public class LoginPageViewModel : BaseViewModel
+    {
+        public LoginPageViewModel()
+        {
+
+        }
+    }
+}
