b71b4cde25de1699333ee98c32157d9bcb2a3477
Enmanuel Toribio <enmanuel@torib.io>
2021-02-19 09:50:45 -0400

    Added icons for ios

--- src/Vacunacion/SisVac.Android/Resources/Resource.designer.cs
+++ src/Vacunacion/SisVac.Android/Resources/Resource.designer.cs
@@ -20832,10 +20832,7 @@ namespace SisVac.Droid
 			public const int icon = 2131492864;
 			
 			// aapt resource value: 0x7F0C0001
-			public const int icon_round = 2131492865;
-			
-			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
+			public const int launcher_foreground = 2131492865;
 			
 			static Mipmap()
 			{
