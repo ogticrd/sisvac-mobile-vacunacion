4e427e31d3cff023d5fa6c02dce4c51d9cf9c7b1
luismts <deivi_0326@hotmail.com>
2021-02-19 17:07:58 -0400

    Shared views in check in process added with their basecdesign

---
+++ src/Vacunacion/SisVac/Pages/CheckIn/Views/Shared/DateDetailView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SisVac.Pages.CheckIn.Views.Shared
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DateDetailView : ContentView
+    {
+        public DateDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Vacunacion/SisVac/Pages/CheckIn/Views/Shared/PersonDetailView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SisVac.Pages.CheckIn.Views.Shared
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonDetailView : ContentView
+    {
+        public PersonDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
