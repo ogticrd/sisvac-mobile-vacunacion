33f235c2d9a90239f755a6f54f877fa780f5dd25
Enmanuel Toribio <enmanuel@torib.io>
2021-02-21 19:53:36 -0400

    Added url parametrization

--- src/Vacunacion/SisVac.Android/appcenter-pre-build.sh
+++ src/Vacunacion/SisVac.Android/appcenter-pre-build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 MANIFEST="$BUILD_REPOSITORY_LOCALPATH/src/Vacunacion/SisVac.Android/Properties/AndroidManifest.xml"
+APPSETTINGS="$BUILD_REPOSITORY_LOCALPATH/src/Vacunacion/SisVac.Android/appsettings.json"
 
 VERSIONNAME=`grep versionName ${MANIFEST} | sed 's/.*versionName\s*=\s*\"\([^\"]*\)\".*/\1/g'`
 
@@ -9,7 +10,12 @@ rm -f ${MANIFEST}.bak
 sed -i.bak "s/android:versionCode="\"1\""/android:versionCode="\"${APPCENTER_BUILD_ID}\""/" ${MANIFEST}
 rm -f ${MANIFEST}.bak
 
+
+sed -i.bak "s/\"CitizenApiBaseUrl\": "\"\""/\"CitizenApiBaseUrl\": "\"${CITIZEN_API_BASE_URL}\""/" ${APPSETTINGS}
+rm -f ${APPSETTINGS}.bak
+
 # Print out file for reference
 cat $MANIFEST
+cat $APPSETTINGS
 
 echo "Updated manifest!"
