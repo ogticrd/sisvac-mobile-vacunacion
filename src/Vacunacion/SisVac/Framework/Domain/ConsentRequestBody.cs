02a4a576722388904ab1dcf67f0308df93f459bd
Enmanuel Toribio <enmanuel@torib.io>
2021-02-22 01:53:50 -0400

    Adding consent and removed n/a option

---
+++ src/Vacunacion/SisVac/Framework/Domain/ConsentRequestBody.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Refit;
+
+namespace SisVac.Framework.Domain
+{
+    public class ConsentRequestBody
+    {
+        [JsonProperty("cedula")]
+        public long Cedula { get; set; }
+
+        [JsonProperty("signature")]
+        IEnumerable<StreamPart> Signature { get; set; }
+
+        [JsonProperty("hasCovid")]
+        public bool HasCovid { get; set; }
+
+        [JsonProperty("isPregnant")]
+        public bool IsPregnant { get; set; }
+
+        [JsonProperty("hadFever")]
+        public bool HadFever { get; set; }
+
+        [JsonProperty("isVaccinated")]
+        public bool IsVaccinated { get; set; }
+
+        [JsonProperty("hadReactions")]
+        public bool HadReactions { get; set; }
+
+        [JsonProperty("isAllergic")]
+        public bool IsAllergic { get; set; }
+
+        [JsonProperty("isMedicated")]
+        public bool IsMedicated { get; set; }
+
+        [JsonProperty("hasTransplant")]
+        public bool HasTransplant { get; set; }
+    }
+}
