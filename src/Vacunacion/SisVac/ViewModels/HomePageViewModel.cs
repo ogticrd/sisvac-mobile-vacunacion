f634a9afd9894eae978a0c444fc20981fda45a45
luismts <deivi_0326@hotmail.com>
2021-02-21 23:47:53 -0400

    Home now recieve the info throw navigation param

--- src/Vacunacion/SisVac/ViewModels/HomePageViewModel.cs
+++ src/Vacunacion/SisVac/ViewModels/HomePageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Prism.Navigation;
 using Prism.Services;
+using SisVac.Framework.Domain;
 
 namespace SisVac.ViewModels
 {
@@ -8,6 +9,14 @@ namespace SisVac.ViewModels
     {
         public HomePageViewModel(INavigationService navigationService, IPageDialogService dialogService) : base(navigationService, dialogService)
         {
+        }
+
+        public new ApplicationUser User { get; set; }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("user"))
+                User = parameters.GetValue<ApplicationUser>("user");
         }
     }
 }
