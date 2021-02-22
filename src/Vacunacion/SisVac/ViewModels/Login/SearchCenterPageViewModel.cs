ddfb4837278b98ff800fbe5ba56b6cd479beea34
luismts <deivi_0326@hotmail.com>
2021-02-21 23:48:14 -0400

    Search functionality added

--- src/Vacunacion/SisVac/ViewModels/Login/SearchCenterPageViewModel.cs
+++ src/Vacunacion/SisVac/ViewModels/Login/SearchCenterPageViewModel.cs
@@ -1,16 +1,45 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 
 namespace SisVac.ViewModels.Login
 {
-    public class SearchCenterPageViewModel : BindableBase
+    public class SearchCenterPageViewModel : BaseViewModel
     {
-        public SearchCenterPageViewModel()
+        public SearchCenterPageViewModel(INavigationService navigationService, IPageDialogService dialogService) : base(navigationService, dialogService)
         {
+            SelectedItemCommand = new DelegateCommand(SelectedItemCommandExecute);
 
+            Centers = new List<string>();
+            Centers.Add("Centro 1");
+            Centers.Add("Centro 2");
         }
+
+        public ICommand SelectedItemCommand { get; set; }
+        public List<string> Centers { get; set; }
+        public int CenterIndexSelected { get; set; }
+
+        private async void SelectedItemCommandExecute()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            var navigationParams = new NavigationParameters
+            {
+                { "selectedItem", Centers[CenterIndexSelected] }
+            };
+
+            await _navigationService.GoBackAsync(navigationParams);
+
+            IsBusy = false;
+        }
+
     }
 }
