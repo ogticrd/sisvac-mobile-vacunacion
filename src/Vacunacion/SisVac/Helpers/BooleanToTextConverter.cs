f4c184852299c68e4f2a35de289b67f26f06beb3
Enmanuel Toribio <enmanuel@torib.io>
2021-02-22 22:02:45 -0400

    Adding converter and starting with vaccination

---
+++ src/Vacunacion/SisVac/Helpers/BooleanToTextConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace SisVac.Helpers
+{
+    public class BooleanToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return "Sí";
+            return "No";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (((String)value) == "Sí");
+        }
+    }
+}
