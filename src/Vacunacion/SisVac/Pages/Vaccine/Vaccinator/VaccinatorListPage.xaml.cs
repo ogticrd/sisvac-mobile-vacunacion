236755998eb10d41303b2ed8606f707c3aafd154
luismts <deivi_0326@hotmail.com>
2021-02-24 12:30:04 -0400

    Vacinnator list base UI added

---
+++ src/Vacunacion/SisVac/Pages/Vaccine/Vaccinator/VaccinatorListPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SisVac.Pages.Vaccinator
+{
+    public partial class VaccinatorListPage : ContentPage
+    {
+        public VaccinatorListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
