48ca6910c5c9f6bfcc76ca40c30de20644258e1b
luismts <deivi_0326@hotmail.com>
2021-02-24 20:34:48 -0400

    Design, Data and validation added to the vaccinator selection process

--- src/Vacunacion/SisVac/ViewModels/Vaccine/TrackingVaccinePageViewModel.cs
+++ src/Vacunacion/SisVac/ViewModels/Vaccine/TrackingVaccinePageViewModel.cs
@@ -1,10 +1,13 @@
-﻿using Prism.Commands;
+﻿using Plugin.ValidationRules;
+using Plugin.ValidationRules.Rules;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using Prism.Services;
 using SisVac.Framework.Domain;
 using SisVac.Framework.Http;
 using SisVac.Framework.Services;
+using SisVac.Helpers.Rules;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +19,9 @@ namespace SisVac.ViewModels.Vaccine
 {
     public class TrackingVaccinePageViewModel : ScanDocumentViewModel
     {
+        List<ApplicationUser> _vaccinatorsList;
+        ApplicationUser _vaccinatorUser;
+
         public TrackingVaccinePageViewModel(
             INavigationService navigationService,
             IPageDialogService dialogService,
@@ -30,6 +36,14 @@ namespace SisVac.ViewModels.Vaccine
             ProgressBarIndicator = 0.0f;
 
             DocumentScanned = async (id) => await GoNextAfterDocumentRead(id);
+
+            VaccinatorSelected = new Validatable<string>();
+            VaccinatorSelected.Validations.Add(new IsNotNullOrEmptyRule() { ValidationMessage = "Necesitas seleccionar un vacunador" });
+
+            VaccinationBatch = new Validatable<string>();
+            VaccinationBatch.Validations.Add(new IsNotNullOrEmptyRule() { ValidationMessage = "Necesitas seleccionar un lote" });
+
+            Init();
         }
         public int PositionView { get; set; }
         public bool IsBackButtonVisible { get; set; } = false;
@@ -45,6 +59,10 @@ namespace SisVac.ViewModels.Vaccine
         }
         public Person Patient { get; set; } = new Person();
         public Consent Consent { get; set; } = new Consent();
+        public Validatable<string> VaccinatorSelected { get; set; }
+        public Validatable<string> VaccinationBatch { get; set; }
+        public List<string> VaccinatorsName { get; set; }
+        public int IndexSelected { get; set; }
 
         public ICommand NextCommand { get; }
         public ICommand ConfirmCommand { get; set; }
@@ -52,12 +70,32 @@ namespace SisVac.ViewModels.Vaccine
 
         public string LotName { get; set; }
 
+        async void Init()
+        {
+            if (_vaccinatorUser == null)
+                _vaccinatorUser = await _cacheService.GetLocalObject<ApplicationUser>(CacheKeyDictionary.VaccinatorDefault);
+
+            if (_vaccinatorsList == null)
+                _vaccinatorsList = await _cacheService.GetLocalObject<List<ApplicationUser>>(CacheKeyDictionary.VaccinatorsList);
+
+            if (_vaccinatorsList?.Count > 0)
+            {
+                VaccinatorsName = _vaccinatorsList.Select(v => v.FullName).ToList();
+                await Task.Run(() => IndexSelected = _vaccinatorsList.FindIndex(v => v.Document == _vaccinatorUser.Document));
+            }
+        }
         private async void OnConfirmCommandExecute()
         {
             if (IsBusy)
                 return;
 
             IsBusy = true;
+
+            if (!VaccinatorSelected.Validate() || !VaccinationBatch.Validate())
+            {
+                IsBusy = false;
+                return;
+            }
 
             var result = await _dialogService.DisplayAlertAsync("Confirmación para la aplicación de dosis", "Primero aplica la dosis de la vacuna al paciente, después confirma la aplicación de la dosis.", "Confirmar", "Cancelar");
             
@@ -136,6 +174,7 @@ namespace SisVac.ViewModels.Vaccine
                     PositionView = 3;
                     break;
                 case 3:
+                    
                     break;
             }
             ProgressBarIndicator = PositionView / 3.0f;
