dc6a0e7a3a73210eb1d07c12c4cc22e5b26c969c
luismts <deivi_0326@hotmail.com>
2021-02-26 09:11:57 -0400

    Use cases added

---
+++ src/Vacunacion/SisVac/Framework/Domain/UseCases/VaccineUseCase.cs
@@ -0,0 +1,87 @@
+﻿using SisVac.Framework.Domain.Dto;
+using SisVac.Framework.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisVac.Framework.Domain.UseCases
+{
+    public interface IVaccineUseCase
+    {
+        Task<Qualification> GetQualificationData(string document);
+        Task<UserResponse> GetDocumentData(string document);
+        Task<Consent> GetConsentData(string document);
+        Task<VaccineTableColumnValues> GetVaccineApplicationData(string document);
+    }
+
+    public class VaccineUseCase : IVaccineUseCase
+    {
+
+        protected ICitizensApiClient _citizensApiClient;
+        public VaccineUseCase(ICitizensApiClient citizensApiClient)
+        {
+            _citizensApiClient = citizensApiClient;
+        }
+
+        public Task<Consent> GetConsentData(string document)
+        {
+            var response = ProcessDocument(document);
+
+            if (!response.isValid)
+                return null;
+
+            return _citizensApiClient.GetConsent(response.document);
+        }
+
+        public Task<UserResponse> GetDocumentData(string document)
+        {
+            var response = ProcessDocument(document);
+
+            if (!response.isValid)
+                return null;
+
+            return _citizensApiClient.GetBasicData(response.document);
+        }
+
+        public Task<Qualification> GetQualificationData(string document)
+        {
+            var response = ProcessDocument(document);
+
+            if (!response.isValid)
+                return null;
+
+            return _citizensApiClient.GetQualification(response.document);
+        }
+
+        public async Task<VaccineTableColumnValues> GetVaccineApplicationData(string document)
+        {
+            var vaccineApplication = await _citizensApiClient.GetVaccineApplication(document);
+            if (vaccineApplication.Citizen != null)
+            {
+                var vaccinator = await GetDocumentData(vaccineApplication.Citizen.Document);
+                return new VaccineTableColumnValues
+                {
+                    Status = "Estatus: APLICADA",
+                    Date = $"Fecha: {vaccineApplication.Date}",
+                    Hour = $"Hora: {vaccineApplication.Hour}",
+                    Center = $"Centro: {vaccineApplication.Location}",
+                    Vaccinator = $"Vacunador: {vaccinator.Name}"
+                };
+            }
+
+            return null;
+        }
+
+        private (string document, bool isValid) ProcessDocument(string document)
+        {
+            if (string.IsNullOrEmpty(document))
+                return (document, false);
+
+            if (document.Length == 13)
+                document = document.Replace("-", "");
+
+            return (document, true);
+        }
+    }
+}
